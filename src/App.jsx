8bd68880918c5177a78e9109e43e1d2bdc100d6b
high5dev <high5dev621@hotmail.com>
2024-10-07 04:27:17 -0400

    tailwind css configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ScrollControls } from "@react-three/drei";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls pages={3} damping={0.1}>
+          <Experience />
+        </ScrollControls>
+      </Canvas>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   );
 }
 
